6fc1bf8ab6dd5bacc4fad2f04c8d578849ca9881
王木又石页 <2545944538@qq.com>
2025-05-11 17:21:46 +0800

    修改问题

--- frontend-admin/src/main.js
+++ frontend-admin/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import router from './routes/'
 // import './style.css'
-import App from './app.vue'
+import App from './App.vue'
 //导入ElementPlus组件
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
