56b73c3f8da1c7ea705234bad0d75dae66589989
王木又石页 <2545944538@qq.com>
2025-05-19 22:33:21 +0800

    Update axios.js

--- frontend-user/src/utils/axios.js
+++ frontend-user/src/utils/axios.js
@@ -4,44 +4,9 @@ import router from '@/routes/index'
 import { localGet } from './index'
 
 
-// 后端接口地址。
 
-// 这边由于后端没有区分测试和正式，姑且都写成一个接口。
-axios.defaults.baseURL = "http://118.31.229.234:5180/"
-// axios.defaults.baseURL = ""
 
-// 携带 cookie，对目前的项目没有什么作用，因为我们是 token 鉴权
-axios.defaults.withCredentials = true
-// 请求头，headers 信息
-axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
-//用localGet
-axios.defaults.headers['token'] = localGet('token') || ''
-// 默认 post 请求，使用 application/json 形式
-axios.defaults.headers.post['Content-Type'] = 'application/json'
-
-//war
-// 添加请求拦截器
-// axios.interceptors.request.use(function (config) {
-//   // 在发送请求之前做些什么
-//   // 根据条件修改请求地址 a就是后端java项目a.war  的文件名
-//     config.url = 'a' + config.url;
-//   return config;
-// }, function (error) {
-//   // 对请求错误做些什么
-//   return Promise.reject(error);
-// });
-
-// 请求拦截器，内部根据返回值，重新组装，统一管理。
-axios.interceptors.response.use(res => {
-
-  if (typeof res !== 'object') {
-    ElMessage.error('服务端异常！')
-    return Promise.reject(res)
   }
-  /*if (res.resultCode != 200) {
-    if (res.data.message) ElMessage.error(res.data.message)
-    if (res.data.resultCode == 419) {
-      router.push({ path: '/login' })
 
 // 响应拦截器
 service.interceptors.response.use(
